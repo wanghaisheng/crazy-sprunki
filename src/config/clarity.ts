2a4c9a3362943d7ddf67f665c5f9e5e6a3c073c8
wanghaisheng <edwin_uestc@163.com>
2024-11-12 19:59:53 +0800

    use data json

---
+++ src/config/clarity.ts
@@ -0,0 +1,6 @@
+// Google tracking
+// - Google Site Verification: for Google Search Console
+// - Google Analytics Measurement ID: for Google Analytics
+// - Google Tag Manager ID: for Google Tag Manager
+
+export const microsoftClarityMeasurementID = 'hh1oixxx'
